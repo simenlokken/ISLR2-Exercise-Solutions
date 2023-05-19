929e8640553fcf0da83bc6d3a2b40597be173ff7
simenlokken <simenloekken97@gmail.com>
2023-05-19 15:06:18 +0200

    Slightly more done

--- Chapter 3 - Linear Regression/Exercises/chapter_3_linear_regression_exercises.R
+++ Chapter 3 - Linear Regression/Exercises/chapter_3_linear_regression_exercises.R
@@ -126,8 +126,55 @@ summary(multiple_carseats_model)
 
 ## B)
 
-# 
+# 1) Price (in thousands). The model indicates that price affect sales of
+# car seats negatively, i.e., when sales increase when the price goes down.
 
+# 2) Urban (qualitative, yes/no, whether the store is in an urban or rural
+# location. The model indicates car seats sales decrease if the store is 
+# located in an urban location. That makes sense because the necessity of a car
+# is normally higher in rural areas due to bigger distances and lack of public
+# transport.
 
+# 3) US (qualitative, yes/no, whether the store is in the US or not). 
+# The model indicates that stores located in the US sell more car seats than 
+# stores located outside of the US. 
+
+## C)
+
+# Not suited for a programming solution.
+
+## D) 
+
+# Price and US is statistically significant at the less than 0.001 level, so if 
+# we can reject H0 and conclude that those variables do affect sales.
+
+## E)
+
+# Fit new model
+
+new_multiple_carseats_model <- 
+  lm(sales ~ price + us, data = carseats)
+
+summary(new_multiple_carseats_model)
+
+## F) 
+
+# I will only consider the last model, model E). 
+
+# The RSE is 2.47, meaning that the sales deviate by 2470 car seats on average 
+# in our model. The R squared is 0.24, meaning that our model explains 24 per 
+# cent of the variance, i.e., there is 76 per cent that is still unexplained.
+# The F statistic is highly statistically significant so price and US is 
+# associated with sales. Altogether, the model does explain some of the variance
+# and we can be quite confident that price and US is associated with sales
+# but there is for sure a lot we do not know about predicting sales of car seats.
+
+## G)
+
+confint(new_multiple_carseats_model)
+
+# See console output for CI's. 
+
+## H)
 
 
